b6b38696d55889d395736ee3a492b4434e25a3b9
amber <a06000208@protonmail.com>
2023-04-16 11:54:17 -0500

    Create gamestate.lua

---
+++ src/lua/autorun/server/gamestate.lua
@@ -0,0 +1,12 @@
+-- https://wiki.facepunch.com/gmod/http
+-- https://wiki.facepunch.com/gmod/hook.Add
+
+
+-- hook.Add("TTTPrepareRound","gb_round_prep", gbPrepareRound)
+-- hook.Add("TTTBeginRound","gb_round_begin", gbBeginRound)
+-- hook.Add("TTTEndRound","gb_round_end", gbEndRound)
+
+-- hook.Add("Initialize", "some_unique_name", )
+-- hook.Add("PlayerConnect", "some_unique_name", )
+-- hook.Add("PlayerDisconnected", "some_unique_name", )
+-- hook.Add("PlayerSay", "Gamestate_Chat_Update", )
